c9724cc1ed1159e09ce6540589769efd3e733af9
dogadinm <110761995+dogadinm@users.noreply.github.com>
2025-06-30 16:28:06 +0200

    created malloc, need bug fix

--- inc/malloc.h
+++ inc/malloc.h
@@ -9,8 +9,8 @@
 # define SMALL 'S'
 # define LARGE 'L'
 
-# define TINY_BLOCK 1024
-# define SMALL_BLOCK 1024 * 32
+# define TINY_CHUNK 1024
+# define SMALL_CHUNK 1024 * 32
 # define MIN_CHUNKS_PER_MAP 100
 
 # define FALSE 0
@@ -23,7 +23,7 @@ void			*g_first_addr;
 typedef struct s_chunk {
 	void         *parent_map;
 	void         *next_chunk;
-	size_t        payload_size;
+	size_t        data_size;
 	unsigned int  available;
 	void         *ptr_data;
 } t_chunk;
@@ -34,5 +34,21 @@ typedef struct s_map {
 	void*      map_next;
 	void*      map_prev;
 	size_t     total_size;
-	char       category;
+	char       type;
 } t_map;
+
+/* map init */
+t_map 	*create_map(size_t size)
+void	init_map(void *first_zone_addr, size_t map_size, char type)
+t_chunk	*init_chunk(t_map *map, void *chunk_ptr, size_t data_size)
+
+t_map	*find_map(t_map *first_map, size_t size)
+
+t_chunk	*add_chunk(t_map *map, size_t size_data)
+
+t_bool	available_chunk_space(t_chunk *chunk, size_t data_size)
+t_chunk	*exist_free_chunk(t_chunk *chunk, size_t data_size)
+
+t_chunk	*last_chunk(t_map *map)
+t_bool	available_map_space(t_map *map, size_t size_data_chunk)
+void	set_chunk(t_chunk *chunk, size_t data_size)
